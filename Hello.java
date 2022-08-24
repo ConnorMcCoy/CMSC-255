71d4496bb0df3af992ddb090e76960dd79c26982
ConnorMcCoy <112022328+ConnorMcCoy@users.noreply.github.com>
2022-08-24 16:03:47 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/***********************************************
+*Hello.java
+************************************************
+Connor McCoy
+*8/24/22
+*CMSC-255-001 
+************************************************/
+public class Hello {
+	public static void main(String[] args) {
+	
+		System.out.println("<Connor>");
+		System.out.println("<Hello Java>");
+		System.out.println("<WindowsOS x64>");
+		System.out.println("<08/24/22>");
+		System.out.println("<CMSC 255-001>");
+		System.out.println();
+	}
+}
